dc2481d7519ca5fe03aeca7aed6ec338819c696f
Barcannn <ovuncbarcan@gmail.com>
2026-09-04 20:14:10 +0300

    Improve appointment scheduling and project documentation

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# clinic-appointment-system-cpp
-Project Freedom - Week 1 
+# Clinic Appointment System(C++)
+
+A console-based clinic appointment management system developed in C++ as an object-oriented programming project.
+
+## Features
+
+- Create appointments using patient name, doctor, date and time
+- Display available time slots for a selected doctor and date
+- Prevent double booking for the same doctor, date, and time
+- List all registered appointments
+- Display doctors' appointment schedules
+- List general appointment hours
+- List doctors and their medical specialties
+- Return to the main menu after each operation
+
+## Doctors
+
+- DR.AHMET - Ear, Nose and Throat
+
+- DR.AYSE - Neurology
+
+- DR.FEDAI - Chest Diseases
+
+- DR.GONUL - Dentistry
+
+- DR.CAKI - Urology
+
+## Available Hours 
+
+- 09:00
+- 10:00
+- 11:00
+- 14:00
+- 15:00
+
+## Concepts Used 
+- Objects-oriented programming
+- Classes and objects 
+- Encapsulation
+- Constructors 
+- `std::vector`
+- Loops and conditional statements 
+- Functions 
+- References and `const`
+- Console input and output 
+
+## Build and Run
+
+Compile:
+
+```bash
+clang++ -std=c++14 main.cpp -o main
+```
+
+Run: 
+
+```bash 
+./main
+```
+
+## Date Format
+
+Enter appointment dates using the following format:
+
+```text
+DD-MM-YYYY
+```
+
+## Future Improvements 
+
+- Input and date validation
+- Saving appointments to a file
+- Appointment cancellation
+- Automated tests 
+- Graphical user interface
+
+
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -2,100 +2,189 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
+// Randevu bilgilerini tutan sınıf
 class Appointment {
 
+private:
+std::string patientName;
+std::string doctorName;
+std::string appointmentDate;
+std::string timeSlot;
+bool isActive;
+public:
+
+Appointment(std::string pName, std::string dName, std::string date, std::string slot)
+: patientName(pName), doctorName(dName), appointmentDate(date),  timeSlot(slot), isActive(true) {}
+void display() const{
+    std::cout << "Hasta: " << patientName
+              << " | Doktor: " << doctorName
+              << " | Tarih: " << appointmentDate
+             << " | Saat: " << timeSlot << std::endl;
+
+}
+
+std::string getTimeSlot() const { return timeSlot; }
+std::string getDoctorName() const { return doctorName;}
+std::string getDate() const { return appointmentDate;
+}
+bool getIsActive() const {
+  return isActive;
+}
+void cancel() {
+  isActive = false;
+}
+};
+ //Randevu yönetimini üstlenen sınıf 
+ class AppointmentManager {
     private:
-    int id;
-    std::string hastaAdi;
-    std::string doktorAdi;
-    std::string tarih;
-    std::string saat;
-    bool aktif;
-    
-    public:
-   
-    Appointment(int pId, std::string hasta, std::string doktor, std::string pTarih, std::string pSaat){
-        id = pId;
-        hastaAdi = hasta;
-        doktorAdi =doktor;
-        tarih = pTarih;
-        saat = pSaat;
-        aktif = true;
-        }
+    std::vector<Appointment> appointments;
+    std::vector<std::string> availableSlots = {"09:00", "10:00", "11:00", "14:00", "15:00"};
 
-    
-    int getId() const { return id; }
-    bool isAktif() const { return aktif;}
-    void iptalEt(){
-        aktif = false;
-    }
-    void bilgiYazdir() const {
-        std::cout <<"ID: " << id
-        << " | Hasta: " <<hastaAdi
-        << " | Doktor: " <<doktorAdi
-        << " | Tarih: " << tarih
-        << " | Saat: " << saat
-        << " | Durum: " << (aktif ? "Aktif" : "Pasif/Iptal")
-        << std :: endl;
-        
-        }
-    };
-    class AppointmentManager {
-        private:
-        std::vector<Appointment> randevular;
-        int sonrakiId = 1;
-        
+      public:
+              void showAvailableSlots(const std::string& doctor, const std::string& date) const {
+                std::cout << "\n---" << doctor 
+                << " | Tarih: " << date
+                << " icin bos saatler ---\n";
+                for (const auto& slot : availableSlots) {
+                  if (!isSlotTaken(doctor, date, slot)) {
+                    std::cout << " - " << slot << "\n";
+                  }
+                }
+              }
+              void showAppointmentHours() const {
+                std::cout << "\n----- Genel Randevu Saatleri ----- \n";
+                for (const auto& slot : availableSlots) {
+                  std::cout << "- " << slot << " \n";
+                }
+              }
+              void showDoctors() const {
+                std::cout << "\n--- Doktorlar Listesi ---\n";
+                std::cout << "DR.AHMET --- Kulak Burun Bogaz\n";
+                std::cout << "DR.AYSE --- Sinir Hastalıklari\n";
+                std::cout << "DR.FEDAİ --- Gögüs Hastalıklari\n";
+                std::cout << "DR.GONUL --- Dis HEKİMİ\n";
+                std::cout << "DR.CAKI --- Uroloji\n";
+              }
+              
+void showDoctorSchedule() const {
+  if(appointments.empty()) {
+    std::cout << "\n Henuz kayitli randevu yok. \n";
+    return;
+  }
+  std::cout << "\n--- Doktorlarin Randevu Saatleri ---\n";
+  for (const auto& app : appointments) {
+    std::cout << "Doktor: " << app.getDoctorName()
+    << " | Tarih: " << app.getDate()
+    << " | Saat: " << app.getTimeSlot() << "\n";
+  }
+}
 
-        public:
-        void randevuOlustur(std::string hasta,
-                            std::string doktor,
-                            std::string tarih,
-                            std::string saat){
-                                randevular.emplace_back(sonrakiId, hasta, doktor, tarih, saat);
-                                sonrakiId++;
-                                std::cout << "[SISTEM] Randevu olusturdu.\n";
-                                }
-        void randevuIptalEt(int randevuId){
-            for (auto& randevu : randevular){
-                if (randevu.getId() == randevuId){
-                if (!randevu.isAktif()) {
-                    std::cout << " [UYARI] Bu randevu zaten daha önce iptal edilmiş.\n";
-                    return;
-                    }
-                    randevu.iptalEt();
-                    std::cout << "[SISTEM] ID " << randevuId << "olan randevu iptal edildi.\n";
-                    return;
-                    }
+ 
+
+
+bool isSlotTaken(const std::string& doctor, const std::string& date, const std::string& slot) const {
+for (const auto& app : appointments){
+if (app.getDoctorName() == doctor &&
+app.getDate() == date &&
+ app.getTimeSlot() == slot &&
+ app.getIsActive())
+ {
+return true; // Aynı doktora aynı saatte randevu var
 }
-std::cout << "[HATA] ID " << randevuId << " olan randevu bulunamadı!\n";
+
 }
-void randevulariYazdir() const {
-    if (randevular.empty()) {
-        std::cout << "[SISTEM] Kayitli randevu bulunmuyor.\n";
-        return;
-    
-    }
-    std::cout << "\n----- TUM RANDEVU LISTESI ---\n";
-    for (const auto& randevu : randevular){
-        randevu.bilgiYazdir();
-    }
-    std::cout << "------------------\n";
+return false;
+
+
+}
+void bookAppointment(){
+std::string pName, dName, date, slot;
+
+std::cout << "HASTA Adı: ";
+std::cin.ignore();
+std::getline(std::cin, pName);
+
+
+std::cout << "DOKTOR Adı (ÖRN: DR. AHMET, DR. AYSE, DR.FEDAİ, DR.GONUL, DR.CAKİ): ";
+std::getline(std::cin, dName);
+std::cout << "Secmek istediginiz tarih (DD-MM-YYYY): ";
+std::getline(std::cin, date);
+showAvailableSlots(dName, date);
+
+
+std::cout << "Seçmek istediginiz saat dilimi: ";
+std::cin >> slot;
+ if(isSlotTaken(dName, date, slot)){
+ std::cout << "\n[HATA] Bu saatte Doktor " << dName << " zaten dolu!\n";
+ }
+ else {
+ appointments.push_back(Appointment(pName, dName, date, slot));
+ std::cout << "\n[BASARILI] Randevunuz basarıyla olusturuldu.\n";
+
+ }
+ }
+ void cancelAppointment() {
+  std::string dName, date, slot;
+  std::cout << " Iptal etmek Istediginiz randevunun DOKTOR ADI: ";
+  std::cin.ignore();
+  std::getline(std::cin, dName);
+  std::cout << " IPTAL ETMEK ISTEDIGINIZ randevunun tarihi (DD-MM-YYYY): ";
+  std::getline(std::cin, date);
+  std::cout << "Iptal etmek istediginiz randevunun saati: ";
+  std::cin >> slot;
+  for (auto& app : appointments){
+    if (app.getDoctorName() == dName &&
+    app.getDate() == date &&
+    app.getTimeSlot() == slot &&
+    app.getIsActive()) {
+      app.cancel();
+      std::cout << "\n[BASARILI] Randevu iptal edildi.\n";
+      return;    }
+  }
+  std::cout << " \n[HATA] Aktif randevu bulunamadi.\n";
+ } // cancelAppointment burada bitiyor
+
+void viewAppointments() const {
+if (appointments.empty()) {
+std::cout << "\nHENUZ KAYITLI RANDEVU YOK.\n";
+return;
+}
+std::cout << "\n----- Guncel Randevu Listesi -----.\n";
+for (const auto& app: appointments) {
+app.display();
+}
 
 }
 };
-int main () {
-    AppointmentManager yonetici;
+int main() {
+AppointmentManager manager;
+int choice;
 
-    yonetici.randevuOlustur("Ahmet Yilmaz","Dr. Ayse Demir","10-06-2026","14:30");
-    yonetici.randevuOlustur("Cevat Agır","Dr. Erdal Ercin","11-06-2026","16.30");
-    yonetici.randevuOlustur("Veysel Sun","Dr. Fedai Barcan","13-06-2026","13.30");
+do {
+std::cout << "\n===== RANDEVU SISTEMI =====\n";
+std::cout << "1. Randevu Al:\n";
+std::cout << "2. Randevulari Listele:\n";
+std::cout << "3. Doktorlarin Randevu Saatlerini Gor:\n";
+std::cout << "4. Genel Randevu Saatlerini Gor:\n";
+std::cout << "5. Doktorlari Listele:\n";
+std::cout << "6. Randevu IPTAL et:\n";
+std::cout << "7. CIKIS YAPIN:\n";
+std::cout << "     Seciminiz:     ";
+std::cin >> choice;
 
-    yonetici.randevulariYazdir();
 
-    yonetici.randevuIptalEt(2);
+switch (choice) {
+case 1: manager.bookAppointment(); break;
+case 2: manager.viewAppointments(); break;
+case 3: manager.showDoctorSchedule(); break;
+case 4: manager.showAppointmentHours();break;
+case 5: manager.showDoctors(); break;
+case 6: manager.cancelAppointment(); break;
+case 7: std::cout << " Sistemden cıkılıyor....\n"; break;
+default: std::cout << " Geçersiz secim! Tekrar Deneyin.\n";
+}
+}while (choice !=7);
+return 0;
+}
 
-    yonetici.randevulariYazdir();
-    return 0;
-    }
+
